f1d0e3a0ee5cac92231e75d9b884a0251522fc42
otarupippi <otarupippi@gmail.com>
2020-10-14 10:58:43 +0900

    made smooth scroll

--- js/firsttry.js
+++ js/firsttry.js
@@ -1,7 +1,28 @@
-// alert('hello');
-// $('#accordionBtn').click(function(){
-//   $('.card-body').toggleClass('jikken');
-// })
+// https://flex-box.net/js-smoothscroll/
+// を参照しました。
+const links = document.querySelectorAll('a[href^="#"]');
+for ( let i = 0; i < links.length; i++ ) {
+  links[i].addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let href = links[i].getAttribute('href');
+      let targetElement = document.getElementById(href.replace('#', ''));
+      console.log(href) 
+
+
+    const elemY = targetElement.getBoundingClientRect().top;
+    const scrollY = window.pageYOffset;
+    const top = elemY + scrollY;
+
+    window.scroll({
+      top: top - 60, 
+      behavior: 'smooth' 
+    });
+    document.getElementById('navbarNavDropdown').classList.remove("show");
+  });
+}
+
+
 $('#accordionBtn').click(function(){
   $('.beneath').toggleClass('hide');
 })
